6c059960dd220ac3e9d114037577587ea30fa87a
Gilles Callebaut, PhD <callebaut.gilles@gmail.com>
2023-04-07 13:11:53 +0200

    Updates from Overleaf

---
+++ code/example-tikz.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use("ggplot")
+
+t = np.arange(0.0, 2.0, 0.1)
+s = np.sin(2 * np.pi * t)
+s2 = np.cos(2 * np.pi * t)
+plt.plot(t, s, "o-", lw=4.1)
+plt.plot(t, s2, "o-", lw=4.1)
+plt.xlabel("time (s)")
+plt.ylabel("Voltage (mV)")
+plt.title("Simple plot $\\frac{\\alpha}{2}$")
+plt.grid(True)
+
+import tikzplotlib
+
+tikzplotlib.save("example-tikz.tex")
